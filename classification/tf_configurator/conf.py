5781b6affb117f1127e721e2026588bff7483f0a
WilliamBar <gratebill@yahoo.gr>
2020-06-03 03:41:19 +0300

    minor update

--- classification/tf_configurator/conf.py
+++ classification/tf_configurator/conf.py
@@ -4,39 +4,41 @@ Confugurator in progress
 
 TO DO
     - Improve Utilities
-    - Set _Conf here
     - add flow control
     - add Method add_data() likewise _Conf
 """
 import tensorflow as tf
-from _base import _Conf
+from _base import _ConfBase, _ConfData
 
-class Configurator(Utilities):
+class Configurator(_ConfBase, _ConfData):
 
-    def __init__(self, inputs, train=True, unbatch=False):
-        if isinstance(inputs, tuple) and len(inputs) is 2:
-            assert all(isinstance(o,tf.data.Dataset) for o in inputs)
-            i = inputs[0]
-            o = inputs[1]
-        elif isinstance(inputs.element_spec, tuple) and len(inputs.element_spec):
-            i = inputs.map(lambda x,_ : x)
-            o = inputs.map(lambda _,y : y)
+    # def __init__(self, inputs, train=True, unbatch=False):
+    #     if isinstance(inputs, tuple) and len(inputs) is 2:
+    #         assert all(isinstance(o,tf.data.Dataset) for o in inputs)
+    #         i = inputs[0]
+    #         o = inputs[1]
+    #     elif isinstance(inputs.element_spec, tuple) and len(inputs.element_spec):
+    #         i = inputs.map(lambda x,_ : x)
+    #         o = inputs.map(lambda _,y : y)
+    #     else:
+    #         raise ValueError('Invalid Dataset/s')
+
+    #     self.path = _set_path()
+    #     if train:
+    #         self.name = lambda x : f'{self.path}{x}_train.tfrecord'
+    #     else:
+    #         self.name = lambda x : f'{self.path}{x}_test.tfrecord'
+
+    #     super().__init__(i,o,unbatch)
+
+    def one_hot(self,label=False):
+        if not hasattr(self,'clses'):
+            self._data_class(label)
+        if label:
+            self.o = self.o.map(lambda x : tf.one_hot(x, self._clses))
         else:
-            raise ValueError('Invalid Dataset/s')
-
-        self.path = _set_path()
-        if train:
-            self.name = lambda x : f'{self.path}{x}_train.tfrecord'
-        else:
-            self.name = lambda x : f'{self.path}{x}_test.tfrecord'
-
-        super().__init__(i,o,unbatch)
-
-    def one_hot(self):
-        if not hasattr(self,'cls'):
-            self.cls = self.classes()
-        self.o = self.o.map(lambda x : tf.one_hot(x, self.cls))
-        return self.o
+            self.i = self.o.map(lambda x : tf.one_hot(x, self._clses))
+        return self
 
     def write_input(self):
         n = self.name('x')
@@ -49,8 +51,3 @@ class Configurator(Utilities):
         s = self.o.map(tf.io.serialize_tensor)
         w = tf.data.experimental.TFRecordWriter(n)
         w.write(s)
-
-    def write_all(self):
-        self.write_config()
-        self.write_input()
-        self.write_output()
