1378e516f02ea223402fcb5c9d7b175407d49c49
WilliamBar <gratebill@yahoo.gr>
2020-06-05 17:51:19 +0300

    update write data

--- classification/tf_configurator/conf.py
+++ classification/tf_configurator/conf.py
@@ -1,53 +1,48 @@
 """
-Confugurator in progress
+Confugurator Class
 @vbar
 
-TO DO
-    - Improve Utilities
-    - add flow control
-    - add Method add_data() likewise _Conf
 """
 import tensorflow as tf
 from _base import _ConfBase, _ConfData
 
+
 class Configurator(_ConfBase, _ConfData):
 
-    # def __init__(self, inputs, train=True, unbatch=False):
-    #     if isinstance(inputs, tuple) and len(inputs) is 2:
-    #         assert all(isinstance(o,tf.data.Dataset) for o in inputs)
-    #         i = inputs[0]
-    #         o = inputs[1]
-    #     elif isinstance(inputs.element_spec, tuple) and len(inputs.element_spec):
-    #         i = inputs.map(lambda x,_ : x)
-    #         o = inputs.map(lambda _,y : y)
-    #     else:
-    #         raise ValueError('Invalid Dataset/s')
+    def set_config(self):
+        """ sets initial configuration """
+        self._conf_param()
 
-    #     self.path = _set_path()
-    #     if train:
-    #         self.name = lambda x : f'{self.path}{x}_train.tfrecord'
-    #     else:
-    #         self.name = lambda x : f'{self.path}{x}_test.tfrecord'
+    def set_data(self,ignore=False):
+        """
+        Sets data attributes in config
+        Param ignore (bool):
+            computes again every attribute
+            ignoring current values
+        """
+        self._set_data(ignore)
 
-    #     super().__init__(i,o,unbatch)
+    def get_path(self):
+        """ returns the path written """
+        return self.pdir
 
-    def one_hot(self,label=False):
-        if not hasattr(self,'clses'):
-            self._data_class(label)
-        if label:
-            self.o = self.o.map(lambda x : tf.one_hot(x, self._clses))
-        else:
-            self.i = self.o.map(lambda x : tf.one_hot(x, self._clses))
-        return self
+    def get_data(self):
+        """ returns input & output saved in the class """
+        return self.i, self.o
 
-    def write_input(self):
-        n = self.name('x')
-        s = self.i.map(tf.io.serialize_tensor)
-        w = tf.data.experimental.TFRecordWriter(n)
-        w.write(s)
-
-    def write_output(self):
-        n = self.name('y')
-        s = self.o.map(tf.io.serialize_tensor)
-        w = tf.data.experimental.TFRecordWriter(n)
-        w.write(s)
+    def write_data(self, one_hot_labels=False, one_hot_features=False):
+        """
+        writes the given data.
+        Params `one_hot_labels`/`one_hot_features (bool)
+            applies one hot encoding before writing them
+        """
+        if one_hot_labels:
+            self.one_hot(True)
+        elif one_hot_features:
+            self.one_hot(False)
+        suffix = self.conf[self.name]['name'] + '.tfrecord'
+        names = [self.pdir+var+suffix for var in ('x','y')]
+        for var,name in zip([self.i,self.o],names):
+            serialised = var.map(tf.io.serialize_tensor)
+            w = tf.data.experimental.TFRecordWriter(name)
+            w.write(serialised)
